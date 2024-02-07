89f87c327d25e1aa8cd621de032890839038881b
Thomas <Thomas_Hudson1@baylor.edu>
2024-02-07 10:11:26 -0600

    final edits

--- employee_report.py
+++ employee_report.py
@@ -3,13 +3,13 @@
 import csv
 
 def main():
-    empdata = open('employee_data.csv','r')
-    #Using csv module, csv file object, important that we do it in two steps
-    csv_file = csv.reader(empdata)
-    next(csv_file)  
+      
 
     def efficiency():
-
+        empdata = open('employee_data.csv','r')
+        #Using csv module, csv file object, important that we do it in two steps
+        csv_file = csv.reader(empdata)
+        next(csv_file)
         #efficiency = productivity / hours worked, (list 5 / list 4)
         high_effic_indv = []
         low_effic_indv = []
@@ -20,7 +20,7 @@ def main():
             elif float(rec[5]) / float(rec[4]) <1:
                 low_effic_indv.append(rec[1])
         
-        print("Highly Efficient Individuals")
+        print(f"Highly Efficient Individuals")
         for name in high_effic_indv:
             print(name)
 
@@ -31,20 +31,47 @@ def main():
     efficiency()
     
     def ages():
+        empdata = open('employee_data.csv','r')
+        #Using csv module, csv file object, important that we do it in two steps
+        csv_file = csv.reader(empdata)
+        next(csv_file)
+       
         #Create three list for the 20s, 30s, and 40s on age
         age20 = []
         age30 = []
         age40 = []
 
-        for rec in csv_file:
-            if float(rec[2]) >= 20 and float(rec[2]) < 30:
-                age20.append(float(rec[2]))
-            elif float(rec[2]) >= 30 and float(rec[2]) < 40:
-                age30.append(float(rec[2]))
-            else: 
-                age40.append(float(rec[2]))
+        for name in csv_file:
+            if 20 <= float(name[2]) < 30:
+                age20.append(name[1])
+            elif  30 <= float(name[2]) < 40:
+                age30.append(name[1])
+            elif 40 <= float(name[2]) < 50:
+                age40.append(name[1])
+        
+        num = 0
     
-        print(age20)
+        print(f'\nEmployees in their 20s')
+        for name in age20:
+            num +=1
+            print(name)
+        print(f'\n Total number of employees in 20s: {num}')
+        
+        num = 0
+        print(f'\nEmployees in their 30s')
+        for name in age30:
+            num +=1
+            print(name)
+        
+        print(f'\n Total number of employees in 30s: {num}')
+        
+        num = 0
+        print(f'\nEmployees in their 40s')
+        for name in age40:
+            num +=1
+            print(name)
+        print(f'\n Total number of employees in 40s: {num}')
+
     ages()
 
 
